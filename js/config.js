0588803d2c9e1518b5d98eac806e95dafcff45b2
ChanPo-Po <tp139365@gmail.com>
2026-06-18 00:30:22 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyHLnjGAnk5FcPLQ58Y3PtyFcWxzKaJj7aJ3l6_hkwvhhaoloELmBI_vJOGICLqLuVJVA/exec';
 const DEMO_MODE = true;
 const DM_LOAI_DICH_VU = ['Sửa chữa','Bảo hành','Đặc quyền','Thay pin miễn phí','Khách nội bộ','Khách đối tác'];
 const DM_DICH_VU = [
